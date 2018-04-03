ea52de5840d2dfb346fa5f0793e0ed70e39cc69f
jeffgong131203 <athlongyj@gmail.com>
2018-04-03 20:17:39 +0800

    201804032017

--- WebPlatform/CloudAPI/YsAPI.cs
+++ WebPlatform/CloudAPI/YsAPI.cs
@@ -14,11 +14,20 @@ namespace WebPlatform.CloudAPI
     {
         private string _appKey = string.Empty;
         private string _secret = string.Empty;
+        private string _subAccountID = string.Empty;
 
         public YsAPI(string appKey, string secret)
         {
             _appKey = appKey;
             _secret = secret;
+        }
+
+        public YsAPI(string subAccountID)
+        {
+            _subAccountID = subAccountID;
+
+            _appKey = System.Configuration.ConfigurationManager.AppSettings["YsAppKey"].ToString();
+            _secret = System.Configuration.ConfigurationManager.AppSettings["YsSecret"].ToString();
         }
 
         /// <summary>
@@ -27,7 +36,7 @@ namespace WebPlatform.CloudAPI
         /// <param name="url"></param>
         /// <param name="parapeters"></param>
         /// <param name="retJson"></param>
-        private void APIError(string url,Dictionary<string,string> parapeters,JObject retJson)
+        private void APIError(string url, Dictionary<string, string> parapeters, JObject retJson)
         { }
 
         /// <summary>
@@ -36,6 +45,7 @@ namespace WebPlatform.CloudAPI
         /// <returns></returns>
         public string getAccessToken()
         {
+
             string url = "https://open.ys7.com/api/lapp/token/get";
 
             Dictionary<string, string> parameters = new Dictionary<string, string>();
@@ -52,10 +62,58 @@ namespace WebPlatform.CloudAPI
             }
             else //API Error,Write Log
             {
-                APIError(url,parameters,jStr);
+                APIError(url, parameters, jStr);
+            }
+
+            //获取子账号AccessToken
+            if (string.IsNullOrEmpty(_subAccountID))
+            {
+                url = "https://open.ys7.com/api/lapp/ram/token/get";
+
+                parameters = new Dictionary<string, string>();
+                parameters.Add("accessToken", retStr);
+                parameters.Add("accountId", _subAccountID);
+
+                jStr = JObject.Parse(HTTPHelper.HttpPostResponse(url, parameters));
+
+                if (jStr["code"].ToString() == "200")
+                {
+                    retStr = jStr["data"]["accessToken"].ToString();
+                }
+                else //API Error,Write Log
+                {
+                    APIError(url, parameters, jStr);
+                }
             }
 
             return retStr;
+        }
+
+        /// <summary>
+        /// 创建子账号
+        /// </summary>
+        /// <param name="accountName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public string addSubAccount(string accountName,string password)
+        {
+            string accountID = string.Empty;
+
+            return accountID;
+        }
+
+        /// <summary>
+        /// 子账号授权
+        /// </summary>
+        /// <param name="accountID"></param>
+        /// <param name="permisson"></param>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        public string SetSubAccountPolicy(string accountID,string[] permisson,string[] resource)
+        {
+            string retCode = string.Empty;
+
+            return retCode;
         }
 
 
@@ -68,7 +126,7 @@ namespace WebPlatform.CloudAPI
             DataTable dtList = new DataTable();
 
             string accessToken = getAccessToken();
-            if(!string.IsNullOrEmpty(accessToken))
+            if (!string.IsNullOrEmpty(accessToken))
             {
                 string url = "https://open.ys7.com/api/lapp/device/list";
 
--- WebPlatform/Controllers/CloudController.cs
+++ WebPlatform/Controllers/CloudController.cs
@@ -5,20 +5,54 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebPlatform.CloudAPI;
+using WebPlatform.Models;
 
 namespace WebPlatform.Controllers
 {
     public class CloudController : Controller
     {
+        private WebPlatformContext db = new WebPlatformContext();
+
         // GET: Cloud
         public ActionResult Index()
         {
             return View();
         }
 
+        private Guid GetUserID()
+        {
+            string userid = string.Empty;
+
+            if (Request.Cookies["User"] != null)
+            {
+                HttpCookie _cookie = Request.Cookies["User"];
+                userid = HttpUtility.UrlDecode(_cookie["UserID"]).ToString();
+
+            }
+
+            return new Guid(userid);
+        }
+
+        //获取萤石直播设备列表
+        /// <summary>
+        /// 获取萤石直播设备列表
+        /// </summary>
+        /// <returns></returns>
         public ActionResult YSLive()
         {
-            YsAPI ys = new YsAPI("1f03ba35bf5d4c92af54a6cd2ee1fe3e", "ecc176d67e0d57f654ec35caa3095cef");
+            YsAPI ys;
+
+            IEnumerable<Cloud_YS_User> ysUserList = db.Cloud_YS_User.Where(userid => userid.UserID == GetUserID()).ToList();
+            string appKey = ysUserList.ToList()[0].YsAppKey;
+            string secret = ysUserList.ToList()[0].YsSecret;
+
+            //为空则是子账号
+            if(string.IsNullOrEmpty(appKey) && string.IsNullOrEmpty(secret))
+            {
+                
+            }
+
+            ys = new YsAPI("1f03ba35bf5d4c92af54a6cd2ee1fe3e", "ecc176d67e0d57f654ec35caa3095cef");
 
             DataTable ysLiveList = ys.getLiveLists();
 
@@ -27,6 +61,12 @@ namespace WebPlatform.Controllers
             return View();
         }
 
+        //获取单个摄像头直播地址
+        /// <summary>
+        /// 获取单个摄像头直播地址
+        /// </summary>
+        /// <param name="serialNo"></param>
+        /// <returns></returns>
         public ActionResult YSLiveVideo(string serialNo)
         {
             YsAPI ys = new YsAPI("1f03ba35bf5d4c92af54a6cd2ee1fe3e", "ecc176d67e0d57f654ec35caa3095cef");
@@ -38,7 +78,7 @@ namespace WebPlatform.Controllers
 
             foreach (DataRow dr in ysLiveList.Rows)
             {
-                if(serialNo == dr["deviceSerial"].ToString())
+                if (serialNo == dr["deviceSerial"].ToString())
                 {
                     rtmpAddress = dr["hdAddress"].ToString();
                     httpAddress = dr["rtmpHd"].ToString();
@@ -51,6 +91,10 @@ namespace WebPlatform.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 多设备直播显示
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult YSLiveVideoMulti()
         {
@@ -65,9 +109,9 @@ namespace WebPlatform.Controllers
 
             foreach (DataRow dr in ysLiveList.Rows)
             {
-                foreach(KeyValuePair<int ,string> sno in serialNos)
+                foreach (KeyValuePair<int, string> sno in serialNos)
                 {
-                    if(sno.Value == dr["deviceSerial"].ToString())
+                    if (sno.Value == dr["deviceSerial"].ToString())
                     {
                         liveAddressList.Add(sno.Key, new string[2] { dr["hdAddress"].ToString(), dr["rtmpHd"].ToString() });
                     }
@@ -80,6 +124,13 @@ namespace WebPlatform.Controllers
             return View();
         }
 
+
+
+        /// <summary>
+        /// 多设备直播监控设置
+        /// </summary>
+        /// <param name="splitNum"></param>
+        /// <returns></returns>
         public ActionResult YSLiveVideoMultiSet(int splitNum)
         {
             YsAPI ys = new YsAPI("1f03ba35bf5d4c92af54a6cd2ee1fe3e", "ecc176d67e0d57f654ec35caa3095cef");
